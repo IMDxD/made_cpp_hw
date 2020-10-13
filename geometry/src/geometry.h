2ba3ea12ce27edfc0a36aa3a83f09fa991570f29
IMDxD <nestor969@mail.ru>
2020-10-13 13:46:19 +0300

    add comparasion add scale and rotation

--- geometry/src/geometry.h
+++ geometry/src/geometry.h
@@ -2,6 +2,15 @@
 #include "vector"
 #include "cmath"
 
+struct Point;
+struct Line;
+class Shape;
+class Polygon;
+class Ellipse;
+class Circle;
+class Rectangle;
+class Square;
+
 struct Point {
 
  public:
@@ -17,18 +26,28 @@ struct Point {
     return !((*this) == right);
   }
 
-  Point reflex(Point& target) {
+  Point reflex(Point &target) {
     double new_x = 2 * this->x - target.x;
     double new_y = 2 * this->y - target.y;
     return Point(new_x, new_y);
   }
 
-  double length_to_point(Point& target)
-};
+  double distance(Point &target) {
+    return std::sqrt((this->x - target.x) * (this->x - target.x) + (this->y - target.y) * (this->y - target.y));
+  }
 
-double line_length(Point& first, Point& second){
-  return std::sqrt(std::pow((first.x - first.x), 2) + std::pow((second.y - second.y), 2));
-}
+  Point rotate(Point &target, double angle) {
+    double new_x = this->x * std::cos(angle) - this->y * std::sin(angle) + this->x;
+    double new_y = this->x * std::sin(angle) + this->y * std::cos(angle) + this->y;
+    return Point(new_x, new_y);
+  }
+
+  Point scale(Point &target, double coefficient) {
+    double new_x = coefficient * (target.x - this->x) + this->x;
+    double new_y = coefficient * (target.y - this->y) + this->y;
+    return Point(new_x, new_y);
+  }
+};
 
 class Line {
 
@@ -55,47 +74,50 @@ class Line {
     return !((*this) == right);
   }
 
-  Point reflex(Point& target) {
-    double center_x = ((target.x/this->angle + target.y - this->bias) / (this->angle - 1/ this->angle));
+  Point reflex(Point &target) {
+    double center_x = ((target.x / this->angle + target.y - this->bias) / (this->angle - 1 / this->angle));
     double center_y = this->angle * center_x + this->bias;
-    double new_x = 2 * center_x - target.x;
-    double new_y = 2 * center_y - target.y;
-    return Point(new_x, new_y);
+    Point center = Point(center_x, center_y);
+    return center.reflex(target);
   }
 };
 
-class Shape{
+class Shape {
  public:
   virtual double perimeter() = 0;
   virtual double area() = 0;
-  virtual bool operator==(const Shape& another) = 0;
-  virtual void rotate(Point center, double angle) = 0;
-  virtual void reflex(Point center) = 0;
-  virtual void reflex(Line axis) = 0;
-  virtual void scale(Point center, double coefficient) = 0;
+  virtual bool operator==(const Shape &another) = 0;
+  virtual void rotate(Point &center, double angle) = 0;
+  virtual void reflex(Point &center) = 0;
+  virtual void reflex(Line &axis) = 0;
+  virtual void scale(Point &center, double coefficient) = 0;
 };
 
-class Polygon: public Shape{
+class Polygon : public Shape {
  private:
   std::vector<Point> vertices;
  public:
 
-  Polygon(std::vector<Point> new_vertices): vertices(new_vertices){};
+  Polygon(std::vector<Point> new_vertices) : vertices(new_vertices) {};
 
-  unsigned verticesCount() {return this->vertices.size();}
+  unsigned verticesCount() { return this->vertices.size(); }
 
   const std::vector<Point> getVertices() {
     return this->vertices;
   }
 
-  bool operator==(const Polygon& another){
+  bool operator==(const Polygon &another) {
     return this->vertices == another.vertices;
+  }
+
+  bool virtual operator==(const Shape &another) override {
+    return false;
   }
 
   double perimeter() override {
     double p = 0;
-    for (size_t i = 0; i < this->vertices.size(); ++i){
-      double length = line_length(this->vertices[i], this->vertices[(i + 1) % this->vertices.size()]);
+    for (size_t i = 0; i < this->vertices.size(); ++i) {
+      double length = this->vertices[i].distance(this->vertices[(i + 1) % this->vertices.size()]);
       p += length;
     }
     return p;
@@ -103,22 +125,34 @@ class Polygon: public Shape{
 
   double area() override {
     double s = 0;
-    for (size_t i = 0; i < this->vertices.size() - 1; ++i){
+    for (size_t i = 0; i < this->vertices.size() - 1; ++i) {
       s += (this->vertices[i].x * this->vertices[(i + 1) % this->vertices.size()].y -
           this->vertices[i].y * this->vertices[(i + 1) % this->vertices.size()].x);
     }
-    return fabs(s)/2;
+    return fabs(s) / 2;
   };
 
-  void reflex(Point center) override {
-    for (size_t i = 0; i < this -> vertices.size(); ++i){
+  void reflex(Point &center) override {
+    for (size_t i = 0; i < this->vertices.size(); ++i) {
       vertices[i] = center.reflex(vertices[i]);
     }
   }
 
-  void reflex(Line axis) override{
-    for (size_t i = 0; i < this -> vertices.size(); ++i){
+  void reflex(Line &axis) override {
+    for (size_t i = 0; i < this->vertices.size(); ++i) {
       vertices[i] = axis.reflex(vertices[i]);
+    }
+  }
+
+  void rotate(Point &center, double angle) override {
+    for (size_t i = 0; i < this->vertices.size(); ++i) {
+      vertices[i] = center.rotate(vertices[i], angle);
+    }
+  }
+
+  void scale(Point &center, double coefficient) override {
+    for (size_t i = 0; i < this->vertices.size(); ++i) {
+      vertices[i] = center.scale(vertices[i], coefficient);
     }
   }
 };
